f1586b681fd5243d46200321c3482d1a9875494b
Pierre Magnien <Magniendcpierre@gmail.com>
2021-09-30 17:15:13 +0200

    4

--- src/connexion/login/Login.js
+++ src/connexion/login/Login.js
@@ -1,83 +1,41 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './Login.css';
+import fusee from '../../images/fusee.svg'
 
-
-const validate = values => {
-    const errors = {};
-    if (!values.firstName) {
-        errors.firstName = 'Required';
-    } else if (values.firstName.length > 15) {
-        errors.firstName = 'Must be 15 characters or less';
-    }
-
-    if (!values.lastName) {
-        errors.lastName = 'Required';
-    } else if (values.lastName.length > 20) {
-        errors.lastName = 'Must be 20 characters or less';
-    }
-
-    if (!values.email) {
-        errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
-
-    return errors;
-};
 
 const LoginBox = () => {
-    // Note that we have to initialize ALL of fields with values. These
-    // could come from props, but since we don’t want to prefill this form,
-    // we just use an empty string. If we don’t do this, React will yell
-    // at us.
     const formik = useFormik({
         initialValues: {
             firstName: '',
             lastName: '',
             email: '',
         },
-        validate,
+        validationSchema: Yup.object({
+            email: Yup.string().email('Invalid email address').required('Required'),
+            password: Yup.string()
+
+                .min(8, 'Must be 8 characters or more')
+                .required('Required'),
+        }),
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
         },
     });
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="firstName">First Name</label>
-            <input
-                id="firstName"
-                name="firstName"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.firstName}
-            />
-            {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+        <div className="login">
+            <form onSubmit={formik.handleSubmit} >
+                <h3>Connexion</h3>
+                <input id="email" placeholder="Adresse email..." type="email" {...formik.getFieldProps('email')} />
+                {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
-            <label htmlFor="lastName">Last Name</label>
-            <input
-                id="lastName"
-                name="lastName"
-                type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.lastName}
-            />
-            {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+                <input id="password" placeholder="Mot de passe..." type="text" {...formik.getFieldProps('password')} />
+                {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
 
-            <label htmlFor="email">Email Address</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-            />
-            {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
-
-            <button type="submit">Submit</button>
-        </form>
+                <button type="submit"><img className="fusee" src={fusee} alt="#" /></button>
+            </form>
+        </div>
     );
 };
 export default LoginBox;
